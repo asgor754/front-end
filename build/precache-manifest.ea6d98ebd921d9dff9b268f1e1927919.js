35214e9d3d88991fe0b7c77be8aad556ba3016f1
ali asgor <aliasgorapu@gmail.com>
2022-08-05 15:25:02 +0600

    frist

---
+++ build/precache-manifest.ea6d98ebd921d9dff9b268f1e1927919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "77d9a60aae8b724e7bb0",
+    "url": "/static/css/main.47dd4e79.chunk.css"
+  },
+  {
+    "revision": "77d9a60aae8b724e7bb0",
+    "url": "/static/js/main.77d9a60a.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "f8647b5f421d2888515e",
+    "url": "/static/css/2.c5c4e7d6.chunk.css"
+  },
+  {
+    "revision": "f8647b5f421d2888515e",
+    "url": "/static/js/2.f8647b5f.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "fdcfcab5381f79b025ed6e4a5e4a9d05",
+    "url": "/index.html"
+  }
+];
